670be6360721170e57461bfefcafb513370297fd
Krishantha Dinesh <krishantha@krishantha.com>
2020-04-24 04:44:05 +0530

    update the message

--- src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
+++ src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApplicationController {
 
-
     @RequestMapping("/app/helloworld")
     public Response getGreeting() {
 
